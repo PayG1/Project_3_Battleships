ad2cde4277fc85d27492bac694904f054a02bb78
PayG1 <micah.athe.009@gmail.com>
2022-08-12 15:38:48 +0000

    Added spaces to the board so it's less clogged

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+from random import randint
+""" The sea reperesents the board of the game """
+
+sea = []
+
+for i in range(8):
+    sea.append(["~"] * 8)
+
