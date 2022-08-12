5e35a32e04d10ee2bdc208afbcc3114dd6b64558
PayG1 <micah.athe.009@gmail.com>
2022-08-12 15:48:28 +0000

    Added Intorduction text and added more space to the board

--- run.py
+++ run.py
@@ -4,10 +4,19 @@
 
 
 from random import randint
+
 """ The sea reperesents the board of the game """
 
 sea = []
 
+""" Made the board more spacious"""
 for i in range(8):
     sea.append(["~"] * 8)
+
+def print_sea(sea):
+    for row in sea:
+        print ("  ".join(row))
+
+print("Wake up! We are being attacked, you are the only one who can use the cannons. We have 5 cannon balls left,try your best to hit the enemy ships.")
+print_sea(sea)
 
