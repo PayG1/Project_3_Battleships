544ba0f2c02e0cef71d2db881b2bc5c21f21f267
PayG1 <micah.athe.009@gmail.com>
2022-08-12 16:07:57 +0000

    added ships

--- run.py
+++ run.py
@@ -17,6 +17,16 @@ def print_sea(sea):
     for row in sea:
         print ("  ".join(row))
 
-print("Wake up! We are being attacked, you are the only one who can use the cannons. We have 5 cannon balls left,try your best to hit the enemy ships.")
+print("Wake up! We are being attacked, you are the only one who can use the cannons.\nWe have 10 cannon balls left, try your best to hit the 4 enemy ships.")
 print_sea(sea)
+
+""" Calculates a random row and column """
+
+
+def rand_row(sea):
+    return randint(0, len(sea) - 1)
+
+def rand_column(sea):
+    return randint((0), len(sea) - 1)
+
 
