f9c0a41ef28e57febce9c07eea355348a3908db5
PayG1 <micah.athe.009@gmail.com>
2023-06-30 18:54:19 +0000

    when deploying the app on heroku, i got a index error in line 59. fixed that error

--- run.py
+++ run.py
@@ -1,37 +1,30 @@
 
 from random import randint
-""" The sea reresents the board of the game """
 
+""" The sea represents the board of the game """
 sea = []
 
-""" Made the board more spacious"""
+""" Made the board more spacious """
 for i in range(8):
-    sea.append(["~"] * 7)
-
+    sea.append(["~"] * 8)
 
 def print_sea(sea):
     for row in sea:
         print("  ".join(row))
 
-
 print("""Wake up! We are being attacked,
 you are the only one who can use the cannons.\
-\nUse the cannonballs and hit the  enemy ship.""")
+\nUse the cannonballs and hit the enemy ship.""")
 print_sea(sea)
 
 """ Calculates a random row and column """
-
-
 def rand_row(sea):
     return randint(0, len(sea) - 1)
 
-
 def rand_column(sea):
-    return randint((0), len(sea) - 1)
+    return randint(0, len(sea[0]) - 1)
 
-
-""" Added Ships  """
-
+""" Added Ships """
 ship_row = rand_row(sea)
 ship_column = rand_column(sea)
 second_ship_row = rand_row(sea)
@@ -40,7 +33,6 @@ list_1 = [ship_row, ship_column]
 list_2 = [second_ship_row, second_ship_column]
 
 """ Loop for the player to keep trying to hit the ship """
-
 for guess in range(8):
     print('guess', guess + 1)
     guess_row = input("Guess row:")
@@ -59,9 +51,8 @@ for guess in range(8):
     elif sea[guess_row][guess_column] == "x":
         print("You have already tried that location, go again.")
     else:
-        if (guess_row == ship_row and guess_column == ship_column) or (
-             guess_row == second_ship_row and
-             guess_column == second_ship_column):
+        if (guess_row == ship_row and guess_column == ship_column) or \
+           (guess_row == second_ship_row and guess_column == second_ship_column):
             print("Nice shot! We can escape now")
             break
         else:
