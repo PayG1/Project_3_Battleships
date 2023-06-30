5adbb5792881062a8d066a1c5313dcb0a9aa4d44
PayG1 <micah.athe.009@gmail.com>
2023-06-30 18:11:32 +0000

    1.The code breaks when empty/invalid inputs are entered for row/column numbers. I added inputs where line 51 and 52 were befor so now when the user doens't input code or an alphanumeric value it gives apropiate feedback error.  2. Fixed line too long error (E501), trailing whitespace(W291) and no newline at end of file(W292).

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
 
 from random import randint
-
-""" The sea reperesents the board of the game """
+""" The sea reresents the board of the game """
 
 sea = []
 
@@ -18,8 +13,10 @@ def print_sea(sea):
     for row in sea:
         print("  ".join(row))
 
-print("Wake up! We are being attacked, you are the only one who can use the cannons.\
-\nUse the cannonballs and hit the  enemy ship.")
+
+print("""Wake up! We are being attacked,
+you are the only one who can use the cannons.\
+\nUse the cannonballs and hit the  enemy ship.""")
 print_sea(sea)
 
 """ Calculates a random row and column """
@@ -31,6 +28,7 @@ def rand_row(sea):
 
 def rand_column(sea):
     return randint((0), len(sea) - 1)
+
 
 """ Added Ships  """
 
@@ -44,21 +42,32 @@ list_2 = [second_ship_row, second_ship_column]
 """ Loop for the player to keep trying to hit the ship """
 
 for guess in range(8):
-    print('guess', guess+1)
-    guess_row = int(input("Guess row:"))-1
-    guess_column = int(input("Guess column:"))-1
+    print('guess', guess + 1)
+    guess_row = input("Guess row:")
+    guess_column = input("Guess column:")
 
-    if (guess_row == ship_row and guess_column == ship_column)\
-            or (guess_row == second_ship_row and guess_column == second_ship_column):
-        print("Nice shot! We can escape now")
-        break
+    if not guess_row.isnumeric() or not guess_column.isnumeric() or \
+            guess_row == "" or guess_column == "":
+        print("Invalid input. Please enter numeric values for row and column.")
+        continue
+    guess_row = int(guess_row) - 1
+    guess_column = int(guess_column) - 1
+
+    if (guess_row < 0 or guess_row > 7) or \
+       (guess_column < 0 or guess_column > 7):
+        print("Where are you aiming at, Focus!")
+    elif sea[guess_row][guess_column] == "x":
+        print("You have already tried that location, go again.")
     else:
-        if (guess_row < 0 or guess_row > 7) \
-                or (guess_column < 0 or guess_column > 7):
-            print("Where are you aiming at, Focus!")
+        if (guess_row == ship_row and guess_column == ship_column) or (
+             guess_row == second_ship_row and
+             guess_column == second_ship_column):
+            print("Nice shot! We can escape now")
+            break
         else:
             print("That was close, keep trying")
             sea[guess_row][guess_column] = "x"
     if guess == 7:
         print("Wait, we are out of cannonballs? It's game over.")
-    print_sea(sea)
+
+print_sea(sea)
